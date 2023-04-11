ace511e4c41e117448ab408e99999b0c513cd9ae
luisy9 <107113354+luisy9@users.noreply.github.com>
2023-04-11 11:09:47 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Docs/>
+</template>
+
+<script>
+import Docs from './components/views/docs/docs.component.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Docs
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
